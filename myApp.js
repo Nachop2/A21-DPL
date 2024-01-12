b23c3582e516889d43fd665757b0a19817273f37
Nacho <italiyankina@gmail.com>
2024-01-12 18:28:46 +0000

    change send to json

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ app.get('/now', function(req,res,next){
     req.time = new Date().toString();
     next();
 }, function(req,res){
-    res.send({time:req.time});
+    res.json({time:req.time});
 });
 
 app.get('/:word/echo', function(req,res){
